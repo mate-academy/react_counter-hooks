6bb153d0633ee6803aae52187eeb4cae03236391
Ashton <nonlocality@gmail.com>
2023-03-18 16:00:18 +0200

    Solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.scss';
 
-export const App: React.FC = () => {
-  const count = 0;
+export const App = () => {
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // increase count by 1
+    setCount((prevCount) => prevCount + 1);
   };
 
   const add100 = () => {
-    // increase count by 100
+    setCount((prevCount) => prevCount + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
     addOne();
 
@@ -26,15 +25,12 @@ export const App: React.FC = () => {
       <h1 className="App__title">
         {`Count: ${count}`}
       </h1>
-
       <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
-
       <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
-
       <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
